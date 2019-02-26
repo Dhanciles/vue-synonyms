7464fbde9b61fc621b63251df89b2d693e209e70
Derek Hanciles <drhanciles@icloud.com>
2019-02-26 10:41:29 -0700

    Implemented api key and created method that will be called once the user has submitted search

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,16 +1,22 @@
 <template>
   <form>
-    <input type="text" class="search" name="search" placeholder="Search" v-model="search"/>
+    <input type="text" class="search" name="search" placeholder="Search" v-model="search" v-on:keyup.enter="searchForSynonyms"/>
   </form>
 </template>
 
 <script> 
+import { key }  from '../constants.js'
 export default {
   name: 'Form',
   data: function () {
     return {
       search: ''
     } 
+  }, 
+  methods: {
+    searchForSynonyms: function (search) {
+      // console.log(data.search)
+    }
   }
 }
 
