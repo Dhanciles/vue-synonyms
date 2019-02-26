f90d4eb389c224ef25f2e4846957890d02cb2fae
Derek Hanciles <drhanciles@icloud.com>
2019-02-26 11:08:07 -0700

    Implemented fetch method for requesting data from api

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,16 +6,21 @@
 
 <script> 
 import { key }  from '../constants.js'
+
 export default {
   name: 'Form',
   data: function () {
     return {
-      search: ''
+      search: '',
+      results: []
     } 
   }, 
   methods: {
     searchForSynonyms: function (search) {
-      // console.log(data.search)
+      fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${this.search}?${key}`)
+      .then(response => response.json())
+      .then(results => this.results = results.syns)
+      .catch(error => console.log(error))
     }
   }
 }
